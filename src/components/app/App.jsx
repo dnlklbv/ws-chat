b833d260a275808e833379f974896296af6aee60
dnlklbv <hi.daniil.k@gmail.com>
2019-08-14 15:46:47 +0300

    feat: implement chat header

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 import { ThemeProvider } from '@livechat/ui-kit';
 import { Container } from '@material-ui/core';
 
+import { Header } from '../header';
+
 import './App.css';
 
 export const App = () => (
   <ThemeProvider>
     <Container maxWidth="xs" style={{padding: 0}}>
+      <Header />
     </Container>
   </ThemeProvider>
 );
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TitleBar } from '@livechat/ui-kit';
+
+export const Header = () => (
+  <TitleBar
+    className="header"
+    title="The Rolling Scopes Chat"
+    style={{width: 'auto'}}
+  />
+)
