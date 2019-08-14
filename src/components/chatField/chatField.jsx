e92e40ec0a02dd7e390b66e2360c436bf9be9343
dnlklbv <hi.daniil.k@gmail.com>
2019-08-14 16:41:14 +0300

    feat: add fake messages

--- src/components/chatField/chatField.jsx
+++ src/components/chatField/chatField.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import { MessageList } from '@livechat/ui-kit';
 
+const messages = [
+  {
+    from: 'Daniil',
+    message: 'Hello world',
+    id: '13as1d12e2',
+    time: '1565789955204',
+  },
+  {
+    from: 'Daniil',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    id: '13asd412e2',
+    time: '1565789956204',
+  },
+  {
+    from: 'User',
+    message: 'Some text',
+    id: '13asgd12e2',
+    time: '1565789959204',
+  },
+  {
+    from: 'Daniil',
+    message: 'Hello world',
+    id: '13asd120e2',
+    time: '1565789965204',
+  },
+]
+
 export const ChatField = () => (
   <MessageList>
   </MessageList>
