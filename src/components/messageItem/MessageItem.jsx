325b7b1826ab54a6c5656c8d338eb2e8c02d5ff4
dnlklbv <hi.daniil.k@gmail.com>
2019-08-14 19:37:37 +0300

    fix: add date formatting in message

--- src/components/messageItem/MessageItem.jsx
+++ src/components/messageItem/MessageItem.jsx
@@ -6,8 +6,13 @@ import { MessageText } from '@livechat/ui-kit';
 
 export const MessageItem = (props) => (
   <Row>
-    <Avatar imgUrl={`https://api.adorable.io/avatars/40/${props.from}`}></Avatar>
-    <Message authorName={props.from} date={props.time} >
+    <Avatar
+      imgUrl={`https://api.adorable.io/avatars/40/${props.from}`}
+    />
+    <Message
+      authorName={props.from}
+      date={new Date(+props.time).toString().split(' ')[4].slice(0, 5)}
+    >
       <MessageText>{props.message}</MessageText>
     </Message>
   </Row>
