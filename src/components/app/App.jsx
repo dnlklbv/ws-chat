dd36bd76350f543858a6e02ae76faee63ec53d70
dnlklbv <hi.daniil.k@gmail.com>
2019-08-14 15:45:22 +0300

    feat: add basic layout

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import { ThemeProvider } from '@livechat/ui-kit';
+import { Container } from '@material-ui/core';
+
 import './App.css';
 
 export const App = () => (
-  <>
-  </>
+  <ThemeProvider>
+    <Container maxWidth="xs" style={{padding: 0}}>
+    </Container>
+  </ThemeProvider>
 );
