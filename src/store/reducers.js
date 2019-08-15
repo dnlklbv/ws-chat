34b0f22e6e57acc38e576878e685c22575b189ee
dnlklbv <hi.daniil.k@gmail.com>
2019-08-15 16:21:49 +0300

    fix: sort messages by time

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -12,7 +12,11 @@ const reducer = (state = defaultState, action) => {
   switch (action.type) {
     case ADD_MESSAGES:
       return {
-        ...state, messages: state.messages.concat(action.payload),
+        ...state, messages: state.messages.concat(
+          action.payload.sort((messageA, messageB) => {
+            return messageA.time < messageB.time ? -1 : 1
+          })
+        ),
       };
     case UPDATE_WEBSOCKET_STATUS:
       return {
