d854904f095350b6840137010d06c677aff9595e
dnlklbv <hi.daniil.k@gmail.com>
2019-08-16 22:28:52 +0300

    feat: add messages to send in store

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,17 @@
 export const ADD_MESSAGES = 'ADD_MESSAGES';
+export const CLEAR_MESSAGES = 'CLEAR_MESSAGES';
 export const UPDATE_WEBSOCKET_STATUS = 'UPDATE_WEBSOCKET_STATUS';
 export const SET_USERNAME = 'SET_USERNAME';
+export const ADD_MESSAGE_TO_SEND = 'ADD_MESSAGE_TO_SEND';
+export const CLEAR_MESSAGES_TO_SEND = 'CLEAR_MESSAGES_TO_SEND';
 
 export const addMessages = messages => ({
   type: ADD_MESSAGES,
   payload: messages,
+});
+
+export const clearMessages = () => ({
+  type: CLEAR_MESSAGES,
 });
 
 export const updateWebSocketStatus = status => ({
@@ -15,4 +22,13 @@ export const updateWebSocketStatus = status => ({
 export const setUsername = username => ({
   type: SET_USERNAME,
   payload: username,
+});
+
+export const addMessageToSend = message => ({
+  type: ADD_MESSAGE_TO_SEND,
+  payload: message,
+});
+
+export const clearMessagesToSend = () => ({
+  type: CLEAR_MESSAGES_TO_SEND,
 });
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,5 @@
 import {
-  ADD_MESSAGES, UPDATE_WEBSOCKET_STATUS, SET_USERNAME,
+  ADD_MESSAGES, CLEAR_MESSAGES, UPDATE_WEBSOCKET_STATUS, SET_USERNAME, ADD_MESSAGE_TO_SEND, CLEAR_MESSAGES_TO_SEND,
 } from './actions';
 
 
@@ -7,6 +7,7 @@ const defaultState = {
   messages: [],
   webSocketStatus: 'closed',
   username: localStorage.getItem('username') || '',
+  messagesToSend: [],
 };
 
 const reducer = (state = defaultState, action) => {
@@ -19,11 +20,17 @@ const reducer = (state = defaultState, action) => {
           })
         ),
       };
+    case CLEAR_MESSAGES:
+      return { ...state, messages: [] };
     case UPDATE_WEBSOCKET_STATUS:
       return { ...state, webSocketStatus: action.payload, };
     case SET_USERNAME:
       localStorage.setItem('username', action.payload);
       return { ...state, username: action.payload, };
+    case ADD_MESSAGE_TO_SEND:
+      return { ...state, messagesToSend: [...state.messagesToSend, action.payload] };
+    case CLEAR_MESSAGES_TO_SEND:
+      return { ...state, messagesToSend: [], };
     default: return state;
   }
 };
