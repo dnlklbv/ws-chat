e45682f31b1ef520e0eac117b9c354469e25d502
dnlklbv <hi.daniil.k@gmail.com>
2019-08-17 15:58:36 +0300

    feat: add log out button to the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { TitleBar } from '@livechat/ui-kit';
+import { TitleBar, IconButton, CloseIcon } from '@livechat/ui-kit';
 
 export const Header = () => (
   <TitleBar
     className="header"
     title="The Rolling Scopes Chat"
+    rightIcons={[
+      <IconButton key="close">
+        <CloseIcon />
+      </IconButton>,
+    ]}
     style={{width: 'auto', position: 'sticky', top: 0}}
   />
-)
+);
