aeff1ddb69f089f5c2e6714b5fbdab086f1330a9
dnlklbv <hi.daniil.k@gmail.com>
2019-08-18 23:52:08 +0300

    feat: implement language changing

--- src/components/languageSelector/LanguageSelector.jsx
+++ src/components/languageSelector/LanguageSelector.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useTranslation } from 'react-i18next';
 
 import { Select, MenuItem } from '@material-ui/core';
 
-export const LanguageSelector = () => (
-  <Select value={'en'} style={{ color: '#fff'}}>
-    <MenuItem value={'en'}>en</MenuItem>
-    <MenuItem value={'ru'}>ru</MenuItem>
-  </Select>
-);
+export const LanguageSelector = () => {
+  const { i18n } = useTranslation();
+  const [ lng, setLng ] = useState(i18n.language);
+
+  console.log(i18n.language)
+
+  return (
+    <Select
+      value={lng}
+      onChange={(e) => {
+        const { value } = e.target;
+        setLng(value);
+        i18n.changeLanguage(value);
+      }}
+      style={{ color: '#fff'}}
+    >
+      <MenuItem value={'en'}>en</MenuItem>
+      <MenuItem value={'ru'}>ru</MenuItem>
+    </Select>
+  )
+};
