1a1a766e2215f5959599236b3ab66cea25d2add4
dnlklbv <hi.daniil.k@gmail.com>
2019-08-18 23:51:40 +0300

    feat: add i18n

---
+++ src/i18n.js
@@ -0,0 +1,32 @@
+import i18n from "i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from "react-i18next";
+
+import enTrans from './locales/en.json';
+import ruTrans from './locales/ru.json';
+
+const resources = {
+  en: {
+    translation: enTrans,
+  },
+  ru: {
+    translation: ruTrans,
+  }
+};
+
+console.log(resources);
+
+i18n
+  .use(LanguageDetector) // passes i18n down to react-i18next
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+
+    keySeparator: false,
+
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    }
+  });
+
+  export default i18n;
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/app';
 
+import './i18n'
+
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/reducers';
