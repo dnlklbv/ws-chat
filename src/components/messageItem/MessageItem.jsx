2a4b65ed118077e729052e3424d91fa37a96a2e3
dnlklbv <hi.daniil.k@gmail.com>
2019-08-18 18:11:41 +0300

    fix: adjust width of the message

--- src/components/messageItem/MessageItem.jsx
+++ src/components/messageItem/MessageItem.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 import { Row, Avatar, Message, MessageText } from '@livechat/ui-kit';
 
 export const MessageItem = (props) => (
-  <Row reverse={props.isMy}>
+  <Row
+    reverse={props.isMy}
+  >
     <Avatar
       imgUrl={`https://api.adorable.io/avatars/40/${props.from}`}
     />
-    <Message
-      authorName={props.from}
-      date={new Date(+props.time).toString().split(' ')[4].slice(0, 5)}
+    <div
+      style={{
+        maxWidth: '60%',
+      }}
     >
-      <MessageText
-        style={{
-          background: '#eee',
-          borderRadius: '0.8rem'
-        }}
-      >{props.message}</MessageText>
-    </Message>
+      <Message
+        authorName={props.from}
+        date={new Date(+props.time).toString().split(' ')[4].slice(0, 5)}
+      >
+        <MessageText
+          style={{
+            background: '#eee',
+            borderRadius: '0.8rem',
+            maxWidth: '85%',
+          }}
+        >{props.message}</MessageText>
+      </Message>
+    </div>
   </Row>
 );
