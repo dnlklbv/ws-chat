94e452f3a25690b3a9536a64b2b78c89b1cd2003
dnlklbv <hi.daniil.k@gmail.com>
2019-08-19 11:05:31 +0300

    fix: turn message field into controlled

--- src/components/messageField/MessageField.jsx
+++ src/components/messageField/MessageField.jsx
@@ -3,25 +3,34 @@ import PropTypes from 'prop-types';
 
 import { useTranslation } from 'react-i18next';
 
-import { TextComposer, Row, TextInput, SendButton, IconButton, EmojiIcon } from '@livechat/ui-kit';
+import { TextComposer, Row, SendButton, IconButton, EmojiIcon } from '@livechat/ui-kit';
 import EmojiPicker from 'emoji-picker-react';
-import { Popover } from '@material-ui/core';
+import { Popover, TextField } from '@material-ui/core';
 import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
 
 export const MessageField = ({ addMessageToSend }) => {
   const { t } = useTranslation();
   const [message, changeMessage] = useState('');
 
+  const sendMessage = () => {
+    addMessageToSend(message);
+    changeMessage('');
+  }
+
   return (
     <TextComposer
       style={{position: 'sticky', bottom: 0}}
-      value={message}
-      onChange={e => changeMessage(e.target.value)}
-      onSend={()=>{ addMessageToSend(message) }}
     >
       <Row align="center">
-        <TextInput placeholder={t('Write a message...')} />
-        <SendButton fit />
+        <TextField
+          value={message}
+          onChange={(e) => { changeMessage(e.target.value )}}
+          onKeyPress={(e) => { if (e.key === 'Enter') sendMessage() }}
+          placeholder={t('Write a message...')}
+          fullWidth
+          InputProps={{ disableUnderline: true }}
+        />
+        <SendButton onClick={() => { sendMessage() }} />
       </Row>
       <Row verticalAlign="center" justify="right">
         <PopupState variant="popover" popupId="popup-popover">
