f0c425521ac48bc03352c15094637eae2acd4e17
dnlklbv <hi.daniil.k@gmail.com>
2019-08-29 15:56:35 +0300

    feat: translate app loader

--- src/components/appLoader/AppLoader.jsx
+++ src/components/appLoader/AppLoader.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import { useTranslation } from 'react-i18next';
+
 import { CircularProgress } from '@material-ui/core';
 
-export const AppLoader = () => (
-  <div style={{marginTop: 20, textAlign: 'center'}} >
-    <CircularProgress />
-    <p>connecting...</p>
-  </div>
-);
+export const AppLoader = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div style={{marginTop: 20, textAlign: 'center'}} >
+      <CircularProgress />
+      <p>{t('connecting...')}</p>
+    </div>
+  )
+};
